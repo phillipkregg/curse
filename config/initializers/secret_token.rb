eea8201001ddda3642304af476964d60b4b37a01
phillipkregg <phillipkregg@gmail.com>
2013-03-24 16:13:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curse::Application.config.secret_token = 'a4c4a32c158ce8bcb2da98f07e25648de379211c96f1325056913aca191df0df5df0f450658b129071fe46790b4863c7ca582255eb31ad18963a16e4557ca989'
